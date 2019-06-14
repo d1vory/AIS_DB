c84c704c253dc8ed19c0e2c1bb9fcad61de038ce
olexandr dubchak <dubchakob@gmail.com>
2019-06-14 12:13:26 +0300

    Starting Making Producer

---
+++ AIS_DB6/Views/Producers/ProducerTable.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using System.Windows.Controls;
+
+using AIS_DB6.ViewModels.Producers;
+
+namespace AIS_DB6.Views.Producers
+{
+    /// <summary>
+    /// Логика взаимодействия для ProducerTable.xaml
+    /// </summary>
+    public partial class ProducerTable : Page
+    {
+        public ProducerTable()
+        {
+            InitializeComponent();
+
+            DataContext = new ProducerViewModel();
+        }
+    }
+}
